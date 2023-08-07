1137623f3f0d08fc7d3effccd5cec5c1ff145acf
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-07 18:33:33 +0000

    task4 pycodestyle

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,6 +7,7 @@ from random import uniform
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """a function that is similar to wait_n"""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
