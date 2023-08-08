d02ef7eef386f227f1fa2a3dfd7b4a6b4bc00c4e
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-08 12:26:59 +0000

    task2 again but for the last time

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -10,5 +10,5 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """measures the total runtime and return it."""
     start_time = time.time()
-    await asyncio.gather(*(async_comprehension for _ in range(4)))
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     return (time.time() - start_time())
