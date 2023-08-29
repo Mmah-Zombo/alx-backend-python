a77a4870a7a413df00ec6a932fe9085ab5666527
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-29 02:37:31 +0000

    test one again

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,15 +2,19 @@
 """this is a test file"""
 import unittest
 from parameterized import parameterized
+from typing import Dict, Tuple, List, Any
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """contains unittests for the nestedmap function"""
 
     @parameterized.expand([
-        ({"x": {"y": {"z": "hello"}}}, ["x", "y", "z"], "hello"),
-        ({"a": {"b": {"c": "end"}}}, ["a", "b", "c"], "end"),
-        ({"a": {"b": {"c": "end"}}}, ["a", "b", "b"], KeyError)])
-    def test_access_nested_map(self, dmap, path, result):
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)])
+    def test_access_nested_map(self,
+                               dmap: Dict,
+                               path: Tuple[str],
+                               result: Any) -> None:
         """tests if the access method works"""
         self.assertEqual(access_nested_map(dmap, path), result)
