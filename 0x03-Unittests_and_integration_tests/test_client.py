137eca6312ea69455c45252813ddbcf9b06910b7
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-01 17:50:02 +0100

    string

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -14,12 +14,12 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google", {"login": "google"}),
         ("abc", {"login": "abc"}),
     ])
-    @patch('get_json')
+    @patch("get_json")
     def test_org(self, org: str, output: Dict, get_json: Mock) -> None:
         """test that GithubOrgClient.org returns the correct value"""
         get_json.return_value = output
         goc = GithubOrgClient(org)
-        get_json.assert_called_once_with(f'https://api.github.com/orgs/{org}')
+        get_json.assert_called_once_with(f"https://api.github.com/orgs/{org}")
 
         response = goc.org()
         self.assertEqual(response, output)
