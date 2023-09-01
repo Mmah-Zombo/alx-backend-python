75ddb9d0e255ee6a038b3175b374eb79f600bb12
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-01 18:50:27 +0100

    task5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 """this is a test file"""
 import unittest
-from client import GithubOrgClient
-from parameterized import parameterized
 from typing import Dict
-from unittest.mock import patch, Mock
+from unittest.mock import (
+    MagicMock,
+    Mock,
+    PropertyMock,
+    patch,
+)
+from parameterized import parameterized, parameterized_class
+from requests import HTTPError
+
+from client import (
+    GithubOrgClient
+)
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -17,7 +27,17 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch("client.get_json")
     def test_org(self, org: str, output: Dict, get_json: Mock) -> None:
         """test that org returns the correct value"""
+
         get_json.return_value = output
         goc = GithubOrgClient(org)
         self.assertEqual(goc.org(), output)
         get_json.assert_called_once_with(f"https://api.github.com/orgs/{org}")
+
+    def test_public_repos_url(self) -> None:
+        """test the public_repos_url function"""
+        with patch.object(GithubOrgClient, 'org', new_callable=property) as og:
+            og.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos",
+            }
+        self.assertEqual(GithubOrgClient._public_repos_url(),
+                         "https://api.github.com/users/google/repos",)
