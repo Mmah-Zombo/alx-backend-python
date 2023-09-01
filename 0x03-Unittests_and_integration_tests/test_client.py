ca3db6542fea8b736d0f19a1f3ea52cca043a47c
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-01 17:46:14 +0100

    task4

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""this is a test file"""
+import unittest
+from client import GithubOrgClient
+from parameterized import parameterized
+from typing import Dict
+from unittest.mock import patch, Mock
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """tests for the GithubOrgClient"""
+
+    @parameterized.expand([
+        ("google", {"login": "google"}),
+        ("abc", {"login": "abc"}),
+    ])
+    @patch('get_json')
+    def test_org(self, org: str, output: Dict, get_json: Mock) -> None:
+        """test that GithubOrgClient.org returns the correct value"""
+        get_json.return_value = output
+        goc = GithubOrgClient(org)
+        get_json.assert_called_once_with(f'https://api.github.com/orgs/{org}')
+
+        response = goc.org()
+        self.assertEqual(response, output)
