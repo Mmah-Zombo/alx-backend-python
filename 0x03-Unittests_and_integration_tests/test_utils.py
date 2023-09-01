e32815bd4b746a039b03e04007a841bea449597b
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-01 16:10:48 +0100

    lambda

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -62,7 +62,7 @@ class TestGetJson(unittest.TestCase):
 class TestMemoize(unittest.TestCase):
     """contains tests for the memoize function"""
 
-    def test_memoize(self):
+    def test_memoize(self) -> None:
         """testing memoize"""
         class TestClass:
 
@@ -73,7 +73,8 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as a_method:
+        with patch.object(TestClass, 'a_method',
+                          return_value=lambda: 42) as a_method:
             tclass = TestClass()
             self.assertEqual(tclass.a_property(), 42)
             self.assertEqual(tclass.a_property(), 42)
