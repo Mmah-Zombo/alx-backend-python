9e2edf9e0789d41f49dec4dfbaa7c5fc7a5f345c
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-01 17:55:37 +0100

    changed the order

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,7 +4,7 @@ import unittest
 from client import GithubOrgClient
 from parameterized import parameterized
 from typing import Dict
-from unittest.mock import patch, Mock, MagicMock
+from unittest.mock import patch, Mock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -15,11 +15,11 @@ class TestGithubOrgClient(unittest.TestCase):
         ("abc", {"login": "abc"}),
     ])
     @patch("get_json")
-    def test_org(self, org: str, output: Dict, get_json: MagicMock) -> None:
+    def test_org(self, org: str, output: Dict, get_json: Mock) -> None:
         """test that GithubOrgClient.org returns the correct value"""
         get_json.return_value = output
         goc = GithubOrgClient(org)
+        response = goc.org()
         get_json.assert_called_once_with(f"https://api.github.com/orgs/{org}")
 
-        response = goc.org()
         self.assertEqual(response, output)
